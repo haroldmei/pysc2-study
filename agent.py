9fcf742a46bd74fa684a5f2ad7bc62d8c852e9f5
Harold Mei <harold.mei@wilcom.com>
2019-06-04 20:10:13 +1000

    sss

--- agent.py
+++ agent.py
@@ -82,6 +82,7 @@ flags.DEFINE_integer("parallel", 1, "How many instances to run in parallel.")
 
 flags.DEFINE_bool("save_replay", True, "Whether to save a replay at the end.")
 
+flags.DEFINE_string("device", "0", "Device for training.")
 flags.DEFINE_string("map", None, "Name of a map to use.")
 flags.mark_flag_as_required("map")
 
